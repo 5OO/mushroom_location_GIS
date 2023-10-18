6d1795ddd345ec78fa59250adfcfde579c8da47d
5OO <git.hub@myrk.eu>
2023-10-18 20:05:50 +0300

    project setup and initial commit.

---
+++ src/test/java/com/gis/team/MushroomLocationGisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gis.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MushroomLocationGisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
