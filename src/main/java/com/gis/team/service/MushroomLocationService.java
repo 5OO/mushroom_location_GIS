8191d350817a2b8953e94deedc54b9c483e7bf08
5OO <git.hub@myrk.eu>
2023-10-18 21:24:02 +0300

    implemented CRUD (Create, Read, Update, Delete) operations for the service layer

---
+++ src/main/java/com/gis/team/service/MushroomLocationService.java
@@ -0,0 +1,43 @@
+package com.gis.team.service;
+import com.gis.team.model.MushroomLocation;
+import com.gis.team.repository.MushroomLocationRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MushroomLocationService {
+
+    @Autowired
+    private MushroomLocationRepository mushroomLocationRepository;
+
+    public List<MushroomLocation> getAllLocations() {
+        return mushroomLocationRepository.findAll();
+    }
+
+    public Optional<MushroomLocation> getLocationById(Integer id) {
+       return mushroomLocationRepository.findById(id);
+    }
+
+    public List<MushroomLocation> getLocationByName(String name) {
+        return mushroomLocationRepository.findByName(name);
+    }
+
+    public MushroomLocation addLocation(MushroomLocation location) {
+        return mushroomLocationRepository.save(location);
+    }
+
+    public MushroomLocation updateLocation(MushroomLocation location) {
+        if (mushroomLocationRepository.existsById(location.getId())) {
+            return mushroomLocationRepository.save(location);
+        } else {
+            throw new EntityNotFoundException(" MushroomLocation with ID " + location.getId() + " not found.");
+        }
+    }
+
+    public void deleteLocation(Integer id) {
+        mushroomLocationRepository.deleteById(id);
+    }
+}
